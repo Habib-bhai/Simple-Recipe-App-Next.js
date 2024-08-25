8f291cc2911928dab7fe8ae09282a46cd3e49823
Habib-bhai <habibahmed918131@gmail.com>
2024-08-25 22:52:39 +0500

    adjustments

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -6,8 +6,8 @@ import   "@/src/app/main.css"
 export default function About(){
     return <>
     <Navbar/>
-    <h1 className={`firstHeading `}>About Us</h1>
-    <h2 className={`firstHeading `}>Welcome to BROS RECIPES – Your Culinary Haven!</h2>
+    <h1 className=" firstHeading ">About Us</h1>
+    <h2 className=" firstHeading2 ">Welcome to BROS RECIPES – Your Culinary Haven!</h2>
     <p className="aboutUsPara"><br/>
 
 At BROS RECIPES, we believe that cooking is more than just preparing meals; it's a way to connect with loved ones, explore different cultures, and express creativity. Our mission is to inspire home cooks of all skill levels to discover the joy of cooking through simple, delicious, and wholesome recipes.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Hello() {
   return ( <>
   <Navbar/>
   {/* <SideBar styleClass="hide"/> */}
-    <h1 className="firstHeading">POPULAR RECIPES</h1>
+    <h1 className="firstHeadingMain">POPULAR RECIPES</h1>
     <div className="CardParent">
     <Card url="/asset/meatball.png" href="/recipe/popular/meatBall" title="Easy Meat Balls"/>
     <Card url="/asset/steak.png" href="/recipe/popular/steak"  title="Best Steak & Veggies" />
